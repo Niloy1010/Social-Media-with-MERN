2ffc02275e14f9ce73d1e748fe6dc182d49f226a
BiozidNiloy10 <niloy.biozid10@gmail.com>
2020-10-02 20:49:05 -0400

    Model Created for user using mongoose

---
+++ models/User.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    avatar: {
+        type: String,
+        required: true
+    }, 
+    date: {
+        type: String,
+        default: Date.now();
+    },
+
+})
+
+const User = mongoose.model('users', userSchema);
+module.exports = User;
