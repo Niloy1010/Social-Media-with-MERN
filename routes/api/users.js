29c9ead5ebdee7b306825a9dfe055fc6c86b8d08
BiozidNiloy10 <niloy.biozid10@gmail.com>
2020-10-02 21:53:48 -0400

    Users register api created

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
+const gravatar = require("gravatar");
 
+const User = require("../../models/User");
 //@route GET api/users/test
 //@desc TESTS users route
 //@access Public
@@ -9,4 +12,42 @@ router.get("/test", (req, res) => {
     msg: "User works",
   });
 });
+
+//@route POST api/users/register
+//@desc Register Users
+//@access Public
+router.post("/register", (req, res) => {
+  User.findOne({
+    email: req.body.email,
+  })
+    .then((user) => {
+      if (user) {
+        res.status(400).json({ email: "Email already Exists" });
+      } else {
+        const avatar = gravatar.url(req.body.email, {
+          s: "200",
+          r: "pg",
+          d: "mm",
+        });
+        const newUser = new User({
+          name: req.body.name,
+          email: req.body.email,
+          avatar,
+          password: req.body.password,
+        });
+        bcrypt.genSalt(10, (err, salt) => {
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err;
+            newUser.password = hash;
+            newUser
+              .save()
+              .then((user) => res.json(user))
+              .catch((err) => console.log(err));
+          });
+        });
+      }
+    })
+    .catch((err) => console.log(err));
+});
+
 module.exports = router;
--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const keys = require("./config/keys");
+const bodyParser = require("body-parser");
 
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
 const posts = require("./routes/api/posts");
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 //Configure Database
 const db = keys.mongoURI;
