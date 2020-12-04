cf25acf53c127baa88127d07cd8cdd6820a10b3c
BiozidNiloy10 <niloy.biozid10@gmail.com>
2020-12-03 21:25:50 -0500

    Prepared for deployment

--- server.js
+++ server.js
@@ -2,13 +2,14 @@ const express = require("express");
 const mongoose = require("mongoose");
 const keys = require("./config/keys");
 const bodyParser = require("body-parser");
+const path = require("path");
 
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
 const posts = require("./routes/api/posts");
 const passport = require("passport");
 require("./config/passport")(passport);
-var cors = require('cors')
+var cors = require("cors");
 
 const app = express();
 
@@ -36,6 +37,13 @@ app.get("/", (req, res) => {
 app.use("/api/users", users);
 app.use("/api/profile", profile);
 app.use("/api/posts", posts);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
